e13ef134fd6ae1e6025ba3c0f2dc0ed0d0b3061e
CaptAlpha <arhitbose.tagore2020@vitstudent.ac.in>
2023-06-26 22:22:04 +0530

    Added Frontend Changes

--- spotify_flask/app.py
+++ spotify_flask/app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
 import numpy as np
 import pickle
+import pandas as pd
+from spotify import get_track_info, get_token, headers, client_id, client_secret
+
 
 
 app = Flask(__name__)
@@ -47,13 +50,41 @@ def prediction():
             genre = model.predict(x)
             genre = le.inverse_transform(genre)
             return genre[0]
+
+        
         
         
         # Predict the genre of the song
         genre = predict_genre(danceability, energy, key, loudness, mode, speechiness, acousticness, instrumentalness, liveness, valence, tempo, duration_ms)
         print(genre)
-        return render_template('prediction.html', genre=genre)
+        
+        df = pd.read_csv('static/utils/genres_v2.csv')
+        # Get all songs with the predicted genre
+        df = df[df['genre'] == genre]
+        # Get 10 random songs
+        df = df.sample(10)
+        print(df['id'])
+        image_list = df['id'].tolist()
+
+
+        # Get token
+        token = get_token(client_id, client_secret)
+
+        # Get track info
+        track_images = {}
+        for track_id in image_list:
+            track_info = get_track_info(headers, track_id)
+            # print(track_info)
+            # Save the track title and image url
+            track_images[track_info['name']] = [track_info['album']['images'][0]['url'], track_info['artists'][0]['name']]
+        
+
+        return render_template('results.html', genre=genre, image_list=image_list, track_images=track_images)
     return render_template('prediction.html')
+
+@app.route('/results', methods=["GET", "POST"])
+def results():
+    return render_template('results.html')
 
 if __name__ == '__main__':
     # Run on host 6000
